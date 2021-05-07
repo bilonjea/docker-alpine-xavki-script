3b7b0b84b8b61785e9811746eb5193ad602af464
bilonjea <j.bilong@gmail.com>
2021-05-07 22:48:14 +0200

    min changes

--- alpinevm.sh
+++ alpinevm.sh
@@ -1,1 +1,22 @@
-#/usr/bin/sh
+#!/bin/sh
+
+##############################################################################################
+#
+#     Description : Déploiement à la volée de conteneur docker alpine
+#
+#     Auteur : Xavier
+#
+#     Date : 24/07/2020  
+#
+##############################################################################################
+
+
+echo "
+
+Options : 
+   --create : lancer des conteneurs
+   --drop : supprimer les conteneurs crées via le script
+   --infos : caractéristiques des conteneurs (ip, nom, user...)
+   --start : redémarrage des conteneurs
+   --ansible : déploiement arborescence ansible
+"
